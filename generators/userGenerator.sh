c94722571c033f5c6c9080cc9c157dc50c581bf5
Anton Bodnar <bodnar.nixsolutions@gmail.com>
2020-03-24 00:16:54 +0200

    sfsdfdsf

--- generators/userGenerator.sh
+++ generators/userGenerator.sh
@@ -1,12 +1,3 @@
-#curl -X POST -d "organisation=parents&mspId=ParentsMSP" http://ec2-54-224-71-59.compute-1.amazonaws.com:3006/users/admin-enroll
-#curl -X POST -d "organisation=hospital&mspId=HospitalMSP" http://ec2-54-224-71-59.compute-1.amazonaws.com:3006/users/admin-enroll
-#curl -X POST -d "organisation=kindergarten&mspId=KindergartenMSP" http://ec2-54-224-71-59.compute-1.amazonaws.com:3006/users/admin-enroll
-#
-#curl -X POST -d "enrollment_id=user-parents-1&first_name=Ann&last_name=Robson&role=Parents&organisation=parents&mspId=ParentsMSP" http://ec2-54-224-71-59.compute-1.amazonaws.com:3006/users
-#curl -X POST -d "enrollment_id=user-hospital-1&first_name=Alex&last_name=Sikorsky&role=Hospital&organisation=hospital&mspId=HospitalMSP" http://ec2-54-224-71-59.compute-1.amazonaws.com:3006/users
-#curl -X POST -d "enrollment_id=user-kindergarten-1&first_name=David&last_name=Connor&role=Kindergarten&organisation=kindergarten&mspId=KindergartenMSP" http://ec2-54-224-71-59.compute-1.amazonaws.com:3006/users
-
-
 HOST_2=ec2-107-22-75-46.compute-1.amazonaws.com
 HOST_3=ec2-3-91-238-161.compute-1.amazonaws.com
 HOST_4=ec2-107-22-12-137.compute-1.amazonaws.com
